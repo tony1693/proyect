5f3528b26546378da2c4f779b1f768d94f5bf8b4
tony1693 <toak_16@hotmail.com>
2023-12-21 19:34:18 +0100

    proyecto1

--- testImdb.ts
+++ testImdb.ts
@@ -5,10 +5,9 @@ const pelicula2 = new Movie("Trainig day",1995,"Estadounidense","Action");
 
 import {Imdb} from "./imdb";
 const dateBase = new Imdb([]);
-const fs = require ("fs");
+
 const fileJ = "imdbBBDD.json";
 const converString = JSON.stringify(dateBase);
 console.log(converString);
-fs.writeFileSync(fileJ, converString);
-const readDate = fs.readFileSync(fileJ,"utf8");
-const storeDate = JSON.parse(readDate);
+
+
